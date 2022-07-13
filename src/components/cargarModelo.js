5a8c41ce1101ff5c892dffa8dad6dbbb71c6e5ad
Edgar Nevárez <edgar_neva@outlook.com>
2022-07-13 11:31:41 -0500

    .

---
+++ src/components/cargarModelo.js
@@ -0,0 +1,22 @@
+const btnAccionModal = document.querySelector( '#boton-accion-modal' );
+const modal = document.querySelector( '#modal' );
+const modalContainer = document.querySelector( '#modal-container' );
+
+let modelo;
+let modeloCargado = false; 
+
+(async() => {
+    try {
+        console.log( "Cargando modelo..." );
+        modelo = await tf.loadLayersModel( "../modelo/model.json" );
+        console.log( "Modelo cargado" );
+
+        modeloCargado = true;
+        btnAccionModal.innerHTML = 'Cerrar';
+    } catch ( err ) {
+        console.log( err );
+        modeloCargado = false
+        btnAccionModal.innerHTML = 'Reintentar';
+    }
+    btnAccionModal.disabled = false;
+})();
---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+const app = express();
+
+const indexHtmlPath = path.join( __dirname, './views/index.html' );
+
+const PORT = process.env.PORT || '3000';
+
+app.use( express.static( path.join( __dirname, './' ) ) );
+app.use( express.json() );
+app.use( cors({
+    origin: 'http://localhost:3000/'
+}));
+
+
+app.get( '*', ( req, res ) => {
+    res.sendFile( indexHtmlPath );
+});
+
+
+app.listen( PORT, () => {
+    console.log( `Server encendido en puerto ${ PORT }` );
+    console.log( `http://localhost:${ PORT }` );
+});
