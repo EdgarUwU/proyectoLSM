e0687e324fe27c564dfc5be0add585e5fc8e1fbe
Edgar Nevárez <edgar_neva@outlook.com>
2022-07-18 23:11:08 -0500

    .

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const app = express();
 
 const indexHtmlPath = path.join( __dirname, './views/index.html' );
 
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT || '3001';
 
 app.use( express.static( path.join( __dirname, './' ) ) );
 app.use( express.json() );
 app.use( cors({
-    origin: 'http://localhost:3000/'
+    origin: 'http://localhost:3001/'
 }));
 
 
